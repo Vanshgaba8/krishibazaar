12352c5b5c51b552fc7a22987b52f10da5fdf309
Vansh Gaba <vansh8thdec@gmail.com>
2024-10-25 00:00:38 +0530

    market rates logic update

--- lib/screens/Farmer/market_rate.dart
+++ lib/screens/Farmer/market_rate.dart
@@ -14,16 +14,21 @@ class _MarketDataPageState extends State<MarketDataPage> {
   bool _isLoading = false;
   String? _error;
   List<dynamic> _marketData = [];
+  String _searchType = 'Market'; // Default is market
 
-  Future<void> _fetchMarketData(String market) async {
+  // Dropdown options
+  final List<String> _searchOptions = ['Market', 'District'];
+
+  Future<void> _fetchMarketData(String input) async {
     setState(() {
       _isLoading = true;
       _error = null;
     });
 
     final apiKey = '579b464db66ec23bdd00000185dd46a797d846526c170251b775b051';
+    final filterType = _searchType == 'Market' ? 'market' : 'district';
     final apiUrl =
-        'https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=$apiKey&format=json&filters[market]=$market';
+        'https://api.data.gov.in/resource/9ef84268-d588-465a-a308-a864a43d0070?api-key=$apiKey&format=json&filters[$filterType]=$input';
 
     try {
       final response = await http.get(Uri.parse(apiUrl));
@@ -32,7 +37,7 @@ class _MarketDataPageState extends State<MarketDataPage> {
         setState(() {
           _marketData = data['records'];
           if (_marketData.isEmpty) {
-            _error = 'No data found for the specified market.';
+            _error = 'No data found for the specified $_searchType.';
           }
         });
       } else {
@@ -74,10 +79,26 @@ class _MarketDataPageState extends State<MarketDataPage> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            // Dropdown for Market/District selection
+            DropdownButton<String>(
+              value: _searchType,
+              items: _searchOptions.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  _searchType = newValue!;
+                });
+              },
+            ),
+            const SizedBox(height: 10),
             TextField(
               controller: _searchController,
               decoration: InputDecoration(
-                hintText: 'Enter Market Name',
+                hintText: 'Enter ${_searchType} Name',
                 suffixIcon: TextButton(
                   onPressed: _startSearch,
                   child: const Text(
