ecd0461627a89b3362d9d825495e7dbaf5229f53
Vansh Gaba <vansh8thdec@gmail.com>
2025-01-08 16:21:21 +0530

    updated necessary files

--- lib/.env.dart
+++
@@ -1,5 +0,0 @@
-String STRIPE_API_KEY =
-    'pk_test_51Pn1XbP0kMQbQN9Vx6C4x38T2MPc2KNYO0S2idCi1pDwvRm3g0OceIz39E9jmhFqPUsvNUzjOWT3n1V2R2PQFhnq00yBXsNvbl';
-String STRIPE_API_SECRET =
-    'sk_test_51Pn1XbP0kMQbQN9VhlAI9SkkqQgWo7PwwWuQp0ydb2SZylsE9dBM0AsFWksrXvz3JHy7oEWFOYA8CZqDgo3gwVtw00PoSyKf6c';
-static var baseUrl = 'https://backend-krishibazaar.onrender.com/api/v1/';
